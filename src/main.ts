67920153708772789b2b92d8d4efa668aced673d
Artem Shabarov <shabarovartemy@yandex.ru>
2025-04-17 23:53:29 +0300

    Add console log url swagger

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 9000;
+  await app.listen(port);
+  console.log(`Swagger: http://localhost:${port}/api/docs`);
 }
 bootstrap();
