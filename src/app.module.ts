86bb5beb136d21ea4f6f5974455f0e4a7db7f3de
Artem Shabarov <shabarovartemy@yandex.ru>
2025-04-17 19:43:48 +0300

    Replace process.env with ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,30 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ParticipantsModule } from './participants/participants.module';
-import { ConfigModule } from '@nestjs/config';
-import { Participant } from './participants/participants.model';
-import { CookingTask } from './task/task.model';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
 import { EvaluationModule } from './evaluation/evaluation.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRESS_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      models: [Participant, CookingTask],
-      autoLoadModels: true,
-      logging: false,
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        models: [],
+        autoLoadModels: true,
+        synchronize: false,
+        logging: false,
+      }),
     }),
     ParticipantsModule,
     TaskModule,
